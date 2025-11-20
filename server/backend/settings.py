5d2c7406f1cfb3873a77fc0e2dcd8fa304ce4d72
Ana <anayelialmonte@gmail.com>
2025-11-20 16:35:39 -0500

    Fix ALLOWED_HOSTS entry in settings.py

--- server/backend/settings.py
+++ server/backend/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'unsafe-key')
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.railway.app', 'oasis-production-6131.up.railway.app',  'project-oasis-omega.vercel.app', "https://project-oasis-7n5rieeks-anas-projects-259712e6.vercel.app"]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.railway.app', 'oasis-production-6131.up.railway.app',  'project-oasis-omega.vercel.app', "project-oasis-7n5rieeks-anas-projects-259712e6.vercel.app"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
